c9c88ea6296f0ac79f068689a68e20e3c52de77a
thomasvn <thomasnguyen96@gmail.com>
2017-10-28 23:29:19 -0700

    Adding in the initial hardcoded sql tables and values

---
+++ sql/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO Advisors VALUES(1,"Silvia", "Figuiera", "sfigueira@scu.edu", "admin");
+INSERT INTO Advisors VALUES(2,"Darren", "Atkinson", "datkinson@scu.edu", "admin");
+INSERT INTO Advisors VALUES(3,"Ahmed", "Amer", "aamer@scu.edu", "admin");
+INSERT INTO Advisors VALUES(4,"Yi", "Fang", "yfang@scu.edu", "admin");
+INSERT INTO Advisors VALUES(5,"Ronald", "Danielson", "rdanielson@scu.edu", "admin");
+
+INSERT INTO Equivalencies VALUES(1, "Data Structures", "COEN 12", "University of Southern California", "Adv Data Structures", "CSE 12", 1, "This course will be a proper replacement");
+INSERT INTO Equivalencies VALUES(1, "Networks", "COEN 146", "University of Southern California", "Networks", "CSE 146", 1, "This course will be a proper replacement");
