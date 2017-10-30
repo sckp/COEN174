a1baf17c3714191c8184721b0cd95ef348ec38b4
thomasvn <thomasnguyen96@gmail.com>
2017-10-30 14:07:07 -0700

    Small fixes to html pages and updated listing of all courses

--- advisorhome.php
+++ advisorhome.php
@@ -1,89 +1,3 @@
-<!-- <!DOCTYPE html>
-<html>
-<head>
-  <title>SCU</title>
-  <meta charset="utf-8"/>
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
-  <link href='https://fonts.googleapis.com/css?family=Roboto:300,400,700' rel='stylesheet' type='text/css'>
-  <link rel="stylesheet" type="text/css" href="main.css">
-</head>
-
-<body>
-  <header class="container">
-  	<div class="row">
-      <h1 class="col-sm-16">
-        <img src="https://upload.wikimedia.org/wikipedia/en/thumb/a/ad/Santa_Clara_U_Seal.svg/1024px-Santa_Clara_U_Seal.svg.png" height = 60px width = 60px>
-        Santa Clara University
-      </h1>
-      <nav class="col-sm-4 text-right">
-        <a class="btn btn-nav" href="index.html" role="button"><b>HOME</b></a>
-        <a class="btn btn-nav" href="about.html" role="button"><b>ABOUT</b></a>
-        <a class="btn btn-nav" href="contact.html" role="button"><b>CONTACT</b></a>
-      </nav>
-    </div>
-  </header>
-
-  <section class="jumbotron">
-    <div class="container">
-      <div class="row text-center">
-        <h2>COEN Grad Course Equivalency</h2>
-        <h3>ADVISOR HOME PAGE</h3>
-          <div class="tranbox">
-            <p> Search for SCU or other univeristy courses here to check if they have been previously approved for equivalency.</p>
-            <form>
-              <div class="row">
-
-                <form class="col-sm-6" name="advisorsearchscu" method="post" action="advisorsearchscu.php">
-                  <h4>Search for SCU Course</h4>
-                  <label> SCU Course Name Abbreviation: </label><br>
-                  <input name="SCUCourseAbbrv" id="SCUCourseAbbrv" type="text" placeholder="COEN12" required><br>
-                  <button name="submit" id="submit" class="btn btn-success" type="submit">SEARCH</button>
-                </form>
-
-                <form class="col-sm-6">
-                  <h4>Search for non-SCU Course</h4>
-                  University Name:<br>
-                  <input type="text" name="uniname"><br>
-                  Course Title:<br>
-                  <input type="text" name="coursetitle"><br>
-                  <a class="btn btn-nav" href="advisorsearchNONSCU.html" role="button"><b>SEARCH</b></a>
-                </form>
-
-              </div>
-            </form>
-         </div>
-      </div>
-    </div>
-  </section>
-
-  <footer class = "container">
-    <div class="row">
-      <p class="col-sm-4">
-        &copy; 2017 Santa Clara University
-      </p>
-      <ul class="col-sm-8">
-        <li class="col-sm-1">
-                    <a href="https://twitter.com/SantaClaraUniv?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor">
-<img src="https://s3.amazonaws.com/codecademy-content/projects/make-a-website/lesson-4/twitter.svg" alt="SCU Twitter">
-</a>
-
-        </li>
-  			<li class="col-sm-1">
-           <a href="https://www.facebook.com/SantaClaraUniversity/">
-<img src="https://s3.amazonaws.com/codecademy-content/projects/make-a-website/lesson-4/facebook.svg" alt="SCU Facebook">
-</a>
-        </li>
-  			<li class="col-sm-1">
-           <a href="https://www.instagram.com/santaclarauniversity/?hl=en">
-<img src="https://s3.amazonaws.com/codecademy-content/projects/make-a-website/lesson-4/instagram.svg" alt="SCU Instagram">
-</a>
-        </li>
-      </ul>
-    </div>
-  </footer>
-</body>
-</html> -->
-
 <?php
   require 'library.php';
 
@@ -105,7 +19,7 @@
   <header class="container">
     <div class="row">
       <h1 class="col-sm-16">
-        <img src="https://upload.wikimedia.org/wikipedia/en/thumb/a/ad/Santa_Clara_U_Seal.svg/1024px-Santa_Clara_U_Seal.svg.png" height = 60px width = 60px>
+        <img src="https://upload.wikimedia.org/wikipedia/en/thumb/a/ad/Santa_Clara_U_Seal.svg/1024px-Santa_Clara_U_Seal.svg.png" height="60" width="60" alt="SCU-Seal">
         Santa Clara University
       </h1>
       <nav class="col-sm-4 text-right">
--- advisorsearchscu.php
+++ advisorsearchscu.php
@@ -1,32 +1,91 @@
-<?php
-require 'db_config.php';
-require 'library.php';
+<!DOCTYPE html>
+<html>
+<head>
+  <title>SCU</title>
+  <meta charset="utf-8"/>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+  <link href='https://fonts.googleapis.com/css?family=Roboto:300,400,700' rel='stylesheet' type='text/css'>
+  <link rel="stylesheet" type="text/css" href="main.css">
+</head>
 
-// Redirect user to login if cookie is not available
-if (!isset($_COOKIE["advisor"])) {
-  redirect("advisorlogin.html");
-}
+<body>
+  <header class="container">
+  	<div class="row">
+      <h1 class="col-sm-16">
+        <img src="https://upload.wikimedia.org/wikipedia/en/thumb/a/ad/Santa_Clara_U_Seal.svg/1024px-Santa_Clara_U_Seal.svg.png" height="60" width="60" alt="SCU-Seal">
+        Santa Clara University
+      </h1>
+      <nav class="col-sm-4 text-right">
+        <a class="btn btn-nav" href="index.html" role="button"><b>HOME</b></a>
+        <a class="btn btn-nav" href="about.html" role="button"><b>ABOUT</b></a>
+        <a class="btn btn-nav" href="contact.html" role="button"><b>CONTACT</b></a>
+      </nav>
+    </div>
+  </header>
 
-// Connect to database
-$conn = new mysqli($db_host, $db_user, $db_pass, $db_name);
-if (! $conn) {
-  die('Could not connect: ' . mysql_error());
-}
+  <section class="jumbotron">
+    <div class="container text-center">
+      <div class="tranbox">
+        <?php
+        require 'db_config.php';
+        require 'library.php';
 
-// if( !empty($_POST['SCUCourseAbbrv'])) {
-//   $SCUCourseAbbrv = $_POST['SCUCourseAbbrv'];
+        // Redirect user to login if cookie is not available
+        if (!isset($_COOKIE["advisor"])) {
+          redirect("advisorlogin.html");
+        }
 
-  $sql = "SELECT * FROM Equivalencies";
-  $result = $conn->query($sql);
+        // Connect to database
+        $conn = new mysqli($db_host, $db_user, $db_pass, $db_name);
+        if (! $conn) {
+          die('Could not connect: ' . mysql_error());
+        }
 
-  if ($result->num_rows > 0) {
-    echo "<table><tr><th>Equivalency ID</th><th>SCU Course Name</th><th>SCU Course Abbreviation</th><th>NONSCU University Name</th><th>NONSCU Course Name</th><th>NONSCU Course Abbreviation</th><th>Is it approved?</th><th>Notes</th></tr>";
-    while($row = $result->fetch_assoc()) {
-      echo "<tr><td>".$row["equivalency_id"]."</td><td>".$row["scu_course_name"]."</td><td>".$row["scu_course_abbrv"]."</td><td>".$row["nonscu_university_name"]."</td><td>".$row["nonscu_course_name"]."</td><td>".$row["nonscu_course_abbrv"]."</td><td>".$row["approved"]."</td><td>".$row["notes"]."</td></tr>";
-    }
-    echo "</table>";
-  }
-// }
+        // Display results from the table
+        $sql = "SELECT * FROM Equivalencies";
+        $result = $conn->query($sql);
 
-mysqli_close($conn);
-?>
+        if ($result->num_rows > 0) {
+          echo "<table><tr><th>Equivalency ID</th><th>SCU Course Name</th><th>SCU Course Abbreviation</th><th>NONSCU University Name</th><th>NONSCU Course Name</th><th>NONSCU Course Abbreviation</th><th>Is it approved?</th><th>Notes</th></tr>";
+          while($row = $result->fetch_assoc()) {
+            echo "<tr><td>".$row["equivalency_id"]."</td><td>".$row["scu_course_name"]."</td><td>".$row["scu_course_abbrv"]."</td><td>".$row["nonscu_university_name"]."</td><td>".$row["nonscu_course_name"]."</td><td>".$row["nonscu_course_abbrv"]."</td><td>".$row["approved"]."</td><td>".$row["notes"]."</td></tr>";
+          }
+          echo "</table>";
+        }
+
+        mysqli_close($conn);
+        ?>
+      </div>
+      <br>
+      <br>
+      <a class="btn btn-success" href="advisorhome.php">Back</a>
+    </div>
+  </section>
+
+  <footer class = "container">
+    <div class="row">
+      <p class="col-sm-4">
+        &copy; 2017 Santa Clara University
+      </p>
+      <ul class="col-sm-8">
+        <li class="col-sm-1">
+                    <a href="https://twitter.com/SantaClaraUniv?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor">
+<img src="https://s3.amazonaws.com/codecademy-content/projects/make-a-website/lesson-4/twitter.svg" alt="SCU Twitter">
+</a>
+
+        </li>
+  			<li class="col-sm-1">
+           <a href="https://www.facebook.com/SantaClaraUniversity/">
+<img src="https://s3.amazonaws.com/codecademy-content/projects/make-a-website/lesson-4/facebook.svg" alt="SCU Facebook">
+</a>
+        </li>
+  			<li class="col-sm-1">
+           <a href="https://www.instagram.com/santaclarauniversity/?hl=en">
+<img src="https://s3.amazonaws.com/codecademy-content/projects/make-a-website/lesson-4/instagram.svg" alt="SCU Instagram">
+</a>
+        </li>
+      </ul>
+    </div>
+  </footer>
+</body>
+</html>
